e3d580f67c2dfd43752a5931506a6321b89dbce7
andor <latandor@gmail.com>
2022-07-18 17:14:41 -0400

    video background

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-import { useState } from 'react';
 
 function App() {
   // Usestate 
   // This syntax is called JSX = Javascript code 
   // returns a state variable and a function to update it
 
-  const [count, setCount] = useState(0)
   return (
-    <div>
+    <div className='screen'>
+      <video autoPlay muted loop id="myVideo">
+      <source src="https://a2development.s3.amazonaws.com/H264HD1080.mov" type="video/mp4"/>
+      </video>
+      <div className="" id="fs-overlay">
+
+      <img id='header-img'
+      src={'https://1000logos.net/wp-content/uploads/2021/08/Dungeons-and-Dragons-Logo-2014.png'} alt="" />
+      
+      </div>
     </div>
-  );
+  );  
 }
 
 export default App;
